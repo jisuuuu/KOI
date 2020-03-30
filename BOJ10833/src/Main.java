715a547bc6e803daea754f237af1e56665850511
jisuuuu <gjkl55@naver.com>
2020-03-30 17:52:08 +0900

    second commit

--- BOJ10833/src/Main.java
+++ BOJ10833/src/Main.java
@@ -1,9 +1,24 @@
+//»ç°ú
+import java.util.Scanner;
 
 public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int num = sc.nextInt();
+		int total = 0;
+		
+		for(int i = 0; i < num; i++) {
+			int people = sc.nextInt();
+			int apple = sc.nextInt();
+			
+			total += apple % people;
+		}
+		
+		System.out.println(total);
 	}
 
 }
